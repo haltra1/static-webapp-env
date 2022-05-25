0711a756eb46e8ece059d79f5aa4fd1a5b3690ec
haltra1 <2433210@gmail.com>
2022-05-25 17:30:20 -0400

    Add files via upload

---
+++ azure-terraform-ftdoor-apim-fp-cosmosdb/variables.tf
@@ -0,0 +1,43 @@
+variable "location" {
+  description = "The Azure Region in which all resources groups should be created."
+}
+
+variable "location2" {
+  description = "Static web app location."
+}
+
+variable "rg-name" {
+  description = "The name of the resource group"
+}
+
+variable "rg-name2" {
+  description = "The name of the resource group"
+}
+
+variable "cosmosdb_name" {
+  description = "The name of the cosmos db"
+}
+
+variable "fn_storage_account_name" {
+  description = "The name of the storage account name"
+}
+
+variable "fn_app_service_plan_name" {
+  description = "The name of the function app service plan"
+}
+
+variable "function_app_name" {
+  description = "The name of the function app"
+}
+
+variable "apim_name" {
+  description = "name of the APIM"
+}
+
+variable "environment" {
+  description = "The environment of the app"
+}
+
+variable "name" {
+  description = "The name of the static app"
+}
